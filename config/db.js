ba59260b4e0371887be25bff15c6b71783119017
Irene Toxla <toxlairene@gmail.com>
2025-09-02 23:22:18 +0000

    updated database config

--- config/db.js
+++ config/db.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGO_URI;
+const connectDatabase = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("❌ MONGO_URI is not defined in environment variables!");
+    }
 
-function connectDatabase () {
-    mongoose.connect(uri).then(() => console.log("MongoDb connected!")).catch((err) => console.log(err));
-}
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("✅ Database is connected");
+  } catch (error) {
+    console.error("❌ Database connection error:", error.message);
+    process.exit(1); // stop server if DB fails
+  }
+};
 
 export default connectDatabase;
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express"
+import dotenv from "dotenv";
 import diseaseRoute from "./routes/diseaseRoute.js"
 import connectDatabase from "./config/db.js";
 import { notFound } from "./middleware/notFound.js";
 import { errorHandler } from "./middleware/errorHandling.js";
 
 
+dotenv.config();
+
 const PORT = process.env.PORT || 8030
+
 
 const app = express();
 app.use(express.json());
@@ -15,6 +19,6 @@ app.use("/api/disease", diseaseRoute);
 app.use(errorHandler);
 app.use(notFound)
 app.listen(PORT, () => {
-    connectDatabase()
+   connectDatabase();
     console.log(`Port ${PORT} ready for connection`)
 });
